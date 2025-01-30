243842c474576fd4051bb479e61f0e70e0a7fedb
Govardhan5256 <devaraalagovardhan@gmail.com>
2025-01-30 15:35:07 +0530

    second commit

--- src/Cards.js
+++ src/Cards.js
@@ -18,10 +18,10 @@ function Cards() {
               <img src={'https://wallpapercave.com/wp/wp13058420.jpg'} className="card-img-top " alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{card.title}</h5>
-                <p className="card-text">
+                {/* <p className="card-text">
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
-                </p>
+                </p> */}
                 <a href="#" className="btn btn-primary" onClick={goSomeWhere} style={{marginRight:'10px'}}>
                 {loader}
                 download
